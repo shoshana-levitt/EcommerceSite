48c83c3a78a78b3ac2e5639949d285ed81135972
Shoshana Levitt <shoshana.levitt@gmail.com>
2023-02-05 14:10:48 -0500

    changed spacing on AllPuppies, removed Edit and Remove buttons

--- client/features/allpuppies/Allpuppies.js
+++ client/features/allpuppies/Allpuppies.js
@@ -2,16 +2,14 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import Typography from "@mui/material/Typography";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
-import { CardActionArea, Button } from '@mui/material';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import { CardActionArea, Button, Box } from "@mui/material";
 import { fetchAllPuppiesAsync, selectAllPuppies } from "./allpuppiesSlice";
 import { me } from "../auth/authSlice";
 import DeletePuppyButton from "../deletePuppy/DeletePuppyButton";
-
-
 
 const Allpuppies = () => {
   const allPuppies = useSelector(selectAllPuppies);
@@ -28,49 +26,66 @@ const Allpuppies = () => {
 
   return (
     <div>
-      <Typography variant="h3" component="h2" align="center">
+      <Typography variant="h3" component="h2" align="center" sx={{ p: 2 }}>
         Puppies
       </Typography>
-      <Grid container direction="row"
-  justifyContent="center"
-  alignItems="center" spacing={2}>
-      <div>{allPuppies.map((puppy) => (
-          <Grid key={puppy.id} item xs={12} sm={6} md={4} style={{
-            display: "inline-block"
-          }}>
-            <NavLink to={`/puppies/${puppy.id}`}>
-          <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={puppy.photoURL}
-          alt="puppy image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {puppy.name} the {puppy.breed}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </NavLink>
-    {user.isAdmin && (
-            <>
-            <NavLink to={`/puppies/${puppy.id}`}>
-            <Button>Edit</Button>
-            </NavLink>
-            <DeletePuppyButton puppyId={puppy.id}/>
-            </>
-          )}
-    </Grid>
-      ))}</div>
-      </Grid>
-      {user.isAdmin && (
+      <Box sx={{ width: "100%" }}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+          sx={{ p: 2 }}
+        >
+          <div>
+            {allPuppies.map((puppy) => (
+              <Grid
+                key={puppy.id}
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                sx={{ m: 0.5 }}
+                style={{
+                  display: "inline-block",
+                }}
+              >
+                <NavLink to={`/puppies/${puppy.id}`}>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={puppy.photoURL}
+                        alt="puppy image"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {puppy.name} the {puppy.breed}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </NavLink>
+                {/* {user.isAdmin && (
+                <>
+                  <NavLink to={`/puppies/${puppy.id}`}>
+                    <Button>Edit</Button>
+                  </NavLink>
+                  <DeletePuppyButton puppyId={puppy.id} />
+                </>
+              )} */}
+              </Grid>
+            ))}
+          </div>
+        </Grid>
+      </Box>
+      {/* {user.isAdmin && (
         <>
-        <Button>Add New Puppy</Button>
+          <Button>Add New Puppy</Button>
         </>
-      )}
+      )} */}
     </div>
   );
 };
