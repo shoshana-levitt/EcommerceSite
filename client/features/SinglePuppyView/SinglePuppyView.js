09e883c2a6c5d6c31bcb7888a2804fadb50d59c7
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-03-11 16:48:17 -0500

    removed name from CardContent on SinglePuppyView

--- client/features/SinglePuppyView/SinglePuppyView.js
+++ client/features/SinglePuppyView/SinglePuppyView.js
@@ -53,58 +53,55 @@ const SinglePuppyView = () => {
               alignItems="center"
               justifyContent="center"
             >
-              <Stack direction="row" alignItems="center">
-                <Card sx={{ width: 300 }}>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="250"
-                      image={puppy.photoURL}
-                      alt="puppy image"
-                    />
-                    <CardContent>
-                      <Typography
-                        gutterBottom
-                        variant="h5"
-                        component="div"
-                        align="center"
-                      >
-                        {puppy.name} the{" "}
+              <Stack direction="column" alignItems="center">
+                <Stack direction="row" alignItems="center">
+                  <Card sx={{ width: 300 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="300"
+                        image={puppy.photoURL}
+                        alt="puppy image"
+                      />
+                    </CardActionArea>
+                  </Card>
+                  <Box sx={{ width: 500 }}>
+                    <Stack
+                      direction="column"
+                      alignItems="center"
+                      justifyContent="center"
+                      m={2}
+                    >
+                      <Typography variant="h3">
+                        {puppy.name.charAt(0).toUpperCase() +
+                          puppy.name.slice(1)}
+                      </Typography>
+                      <Typography>
+                        Breed:{" "}
                         {puppy.breed.charAt(0).toUpperCase() +
                           puppy.breed.slice(1)}
                       </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-                <Box sx={{ width: 500 }}>
-                  <Stack
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    m={2}
-                  >
-                    <Typography>
-                      Breed:{" "}
-                      {puppy.breed.charAt(0).toUpperCase() +
-                        puppy.breed.slice(1)}
-                    </Typography>
-                    <Typography>
-                      Color:{" "}
-                      {puppy.color.charAt(0).toUpperCase() +
-                        puppy.color.slice(1)}
-                    </Typography>
-                    <Typography>Age: {puppy.age}</Typography>
-                    <Typography>Price: ${puppy.price}</Typography>
-                    <Typography>{puppy.description}</Typography>
-                    <Button
-                      startIcon={<AddShoppingCart />}
-                      variant="contained"
-                      onClick={addToCartHandler}
-                      sx={{ width: 200 }}
-                    >
-                      Add to cart
-                    </Button>
-                  </Stack>
+                      <Typography>
+                        Color:{" "}
+                        {puppy.color.charAt(0).toUpperCase() +
+                          puppy.color.slice(1)}
+                      </Typography>
+                      <Typography>Age: {puppy.age}</Typography>
+                      <Typography>Price: ${puppy.price}</Typography>
+
+                      <Button
+                        startIcon={<AddShoppingCart />}
+                        variant="contained"
+                        onClick={addToCartHandler}
+                        sx={{ width: 200 }}
+                      >
+                        Add to cart
+                      </Button>
+                    </Stack>
+                  </Box>
+                </Stack>
+                <Box sx={{ width: 800 }} m={2}>
+                  <Typography>{puppy.description}</Typography>
                 </Box>
               </Stack>
             </Box>
