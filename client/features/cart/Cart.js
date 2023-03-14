59d2893efd47b23f8304b57a3c7308707775fbc6
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-03-14 13:22:15 -0400

    changed empty cart message size

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -171,7 +171,7 @@ const Cart = () => {
     <>
       {cartToShow[0] === undefined && (
         <Box m={2}>
-          <Typography variant="h3">Your cart is empty.</Typography>
+          <Typography variant="h4">Your cart is empty.</Typography>
         </Box>
       )}
 
