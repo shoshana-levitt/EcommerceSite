7108453c126456f158182fc332d651d90d2ba2db
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-05-05 16:42:56 -0400

    changed copy on Checkout page

--- client/features/checkout/checkout.js
+++ client/features/checkout/checkout.js
@@ -6,7 +6,9 @@ const Checkout = () => {
     <div>
       <Box sx={{ p: 2 }}>
         <Stack justifyContent="center" alignItems="center" spacing={1}>
-          <Typography variant="h3">Your booking was successful!</Typography>
+          <Typography variant="h3">
+            Your puppy playdate booking was successful!
+          </Typography>
           <Typography variant="h6">
             Please check your email for your full order details.
           </Typography>
